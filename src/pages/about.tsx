f9022ba1577a3f07f9aa8d3d3224b4f82653d74b
Tansel Berkant Oflaz <45034831+tanselberkant@users.noreply.github.com>
2023-06-03 17:37:04 +0300

    tutorial part 2 finished

---
+++ src/pages/about.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import Layout from '../components/layout/Layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => (
+    <>
+      <title>About Me</title>
+      <meta name="description" content="Dont know what the hell is this" />
+    </>
+  )
+// Step 3: Export your component
+export default AboutPage
